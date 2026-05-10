0367b9e66ab64019a23e6883b7ccee8491c4fef6
Francisco José Coutiño Morales <charchito077@gmail.com>
2026-05-10 15:35:01 -0600

    fix: eliminar carpeta grafos_nuevo duplicada

--- app/algoritmos/grafos_nuevo/dfs.py
+++
@@ -1,8 +0,0 @@
-def dfs(grafo, inicio, visitados=None):
-    if visitados is None:
-        visitados = []
-    visitados.append(inicio)
-    for vecino in grafo[inicio]:
-        if vecino not in visitados:
-            dfs(grafo, vecino, visitados)
-    return visitados
