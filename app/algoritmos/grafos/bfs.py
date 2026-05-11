b849d73bdcf5b62c5aae85811f8ca7bebf334182
Tlalli <tlalliortega890@gmail.com>
2026-05-10 19:01:44 -0600

    Implementación BFS/DFS en FastAPI, cambios 02

--- app/algoritmos/grafos/bfs.py
+++ app/algoritmos/grafos/bfs.py
@@ -3,12 +3,10 @@ from collections import deque
 def bfs(grafo, inicio):
     visitados = []
     cola = deque([inicio])
-    vistos = set([inicio])
-
+    vistos= set([inicio])
     while cola:
         nodo = cola.popleft()
         visitados.append(nodo)
-
         for vecino in grafo[nodo]:
             if vecino not in vistos:
                 vistos.add(vecino)
